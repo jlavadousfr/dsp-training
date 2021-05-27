787c8caadb4d0c6546d6f30db72416c7af695cb1
Vincent Villet <vincentvillet@MacBook-Pro-de-Vincent.local>
2021-05-27 19:43:20 +0200

    Implement input drift monitoring

--- src/predict_and_monitor.py
+++ src/predict_and_monitor.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from monitor.monitor import monitor
+from monitor.monitor import monitor_loans_ratio, monitor_input_drift
 from predict.predict import predict
 import constants.files as files
 import constants.models as m
@@ -13,14 +13,20 @@ def predict_and_monitor():
     """
     logging.info("*********** 1/5 Loading and splitting data ***********")
 
+    try:
+        monitor_input_drift(test_file_path=files.NEW_LOANS_TO_ACCEPT,
+                            input_stats_history_path=files.INPUT_STATS_HISTORY)
+    except:
+        logging.warning("Input drift detected !")
+
     predict(test_file_path=files.NEW_LOANS_TO_ACCEPT,
             preprocessing_pipeline_path=files.PREPROCESSING_PIPELINE,
             logistic_reg_model_path=os.path.join(files.MODELS, m.LOGISTIC_REG_MODEL_NAME),
             prediction_file_path=files.NEW_PREDICTIONS)
 
-    monitor(prediction_file_path=files.NEW_PREDICTIONS,
-            prediction_history_path=files.PREDICTIONS_HISTORY,
-            metrics_history_path=files.METRICS_HISTORY)
+    monitor_loans_ratio(prediction_file_path=files.NEW_PREDICTIONS,
+                        prediction_history_path=files.PREDICTIONS_HISTORY,
+                        metrics_history_path=files.METRICS_HISTORY)
 
 
 if __name__ == "__main__":
